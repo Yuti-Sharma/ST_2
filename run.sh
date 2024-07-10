9dcc7c0d23fdf1db881f19c23e535fe9f005f445
Yuti Sharma <103135197+Yuti-Sharma@users.noreply.github.com>
2024-07-10 14:35:29 +0530

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,20 @@
+
+1. #Clone the repository:
+    git clone <repository-url>
+
+2. #Navigate to the project directory:
+    
+    cd MagicalArena
+    
+3. #Compile the project:
+    
+    mvn clean compile
+    
+4. #Run the project:
+    
+    mvn exec:java -Dexec.mainClass="arena.Main"
+    
+5. #To run the tests:
+    
+    mvn test
+    
